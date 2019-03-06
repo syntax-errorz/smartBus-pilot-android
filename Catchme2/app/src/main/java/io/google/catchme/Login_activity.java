8de030fcffa19d4b088a5d432c35714c7a19ce44
Anish J <31165122+Anish-Jay@users.noreply.github.com>
2019-03-06 20:38:48 +0530

    App icon

--- Catchme2/app/src/main/java/io/google/catchme/Login_activity.java
+++ Catchme2/app/src/main/java/io/google/catchme/Login_activity.java
@@ -1,8 +1,10 @@
 package io.google.catchme;
 
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,5 +23,12 @@ public class Login_activity extends AppCompatActivity {
         login=(Button)findViewById(R.id.login);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_activity);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent enabler_intent=new Intent(Login_activity.this,Login_activity.class);
+                startActivity(enabler_intent);
+            }
+        });
     }
 }
